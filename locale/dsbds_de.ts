e4a3f73f30cc6294ac86ad5701901496d0a9547a
Marcel <mk@freeshell.de>
2019-09-14 20:42:07 +0200

    Update

--- locale/dsbds_de.ts
+++ locale/dsbds_de.ts
@@ -4,7 +4,7 @@
 <context>
     <name>Brightness</name>
     <message>
-        <location filename="src/brightness.cpp" line="40"/>
+        <location filename="../src/brightness.cpp" line="40"/>
         <source>Software brightness</source>
         <translation>Pseudo-Helligkeit</translation>
     </message>
@@ -12,7 +12,7 @@
 <context>
     <name>DPMS</name>
     <message>
-        <location filename="src/dpms.cpp" line="39"/>
+        <location filename="../src/dpms.cpp" line="39"/>
         <source>Enable DPMS</source>
         <translation>DPMS aktivieren</translation>
     </message>
@@ -20,7 +20,7 @@
 <context>
     <name>LCDBrightness</name>
     <message>
-        <location filename="src/lcdbrightness.cpp" line="40"/>
+        <location filename="../src/lcdbrightness.cpp" line="40"/>
         <source>LCD brightness level</source>
         <translation>LCD-Helligkeitsstufe</translation>
     </message>
@@ -28,53 +28,53 @@
 <context>
     <name>MainWin</name>
     <message>
-        <location filename="src/mainwin.cpp" line="64"/>
+        <location filename="../src/mainwin.cpp" line="64"/>
         <source>DPMS</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="src/mainwin.cpp" line="65"/>
+        <location filename="../src/mainwin.cpp" line="65"/>
         <source>Blanktime</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="src/mainwin.cpp" line="70"/>
-        <location filename="src/mainwin.cpp" line="198"/>
+        <location filename="../src/mainwin.cpp" line="70"/>
+        <location filename="../src/mainwin.cpp" line="205"/>
         <source>&amp;Save</source>
         <translation>&amp;Speichern</translation>
     </message>
     <message>
-        <location filename="src/mainwin.cpp" line="71"/>
+        <location filename="../src/mainwin.cpp" line="71"/>
         <source>&amp;Quit</source>
         <translation>&amp;Beenden</translation>
     </message>
     <message>
-        <location filename="src/mainwin.cpp" line="157"/>
+        <location filename="../src/mainwin.cpp" line="164"/>
         <source>Saved</source>
         <translation>Gespeichert</translation>
     </message>
     <message>
-        <location filename="src/mainwin.cpp" line="192"/>
+        <location filename="../src/mainwin.cpp" line="199"/>
         <source>Settings have been changed</source>
         <translation>Einstellungen wurden geändert</translation>
     </message>
     <message>
-        <location filename="src/mainwin.cpp" line="193"/>
+        <location filename="../src/mainwin.cpp" line="200"/>
         <source>Settings changed</source>
         <translation>Einstellungen geändert</translation>
     </message>
     <message>
-        <location filename="src/mainwin.cpp" line="194"/>
+        <location filename="../src/mainwin.cpp" line="201"/>
         <source>Do you want to save your changes?</source>
         <translation>Möchten Sie die Änderungen speichern?</translation>
     </message>
     <message>
-        <location filename="src/mainwin.cpp" line="200"/>
+        <location filename="../src/mainwin.cpp" line="207"/>
         <source>&amp;Quit without saving</source>
         <translation>&amp;Beenden ohne zu speichern</translation>
     </message>
     <message>
-        <location filename="src/mainwin.cpp" line="201"/>
+        <location filename="../src/mainwin.cpp" line="208"/>
         <source>&amp;Cancel</source>
         <translation>&amp;Abbrechen</translation>
     </message>
@@ -82,27 +82,40 @@
 <context>
     <name>Mode</name>
     <message>
-        <location filename="src/mode.cpp" line="51"/>
+        <location filename="../src/mode.cpp" line="50"/>
         <source>Set mode</source>
         <translation>Setze Mode</translation>
     </message>
 </context>
 <context>
+    <name>OnOff</name>
+    <message>
+        <location filename="../src/onoff.cpp" line="9"/>
+        <source>Enable output</source>
+        <translation>Ausgabe aktivieren</translation>
+    </message>
+</context>
+<context>
     <name>Output</name>
     <message>
-        <location filename="src/output.cpp" line="8"/>
+        <location filename="../src/output.cpp" line="28"/>
         <source>Mode</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="src/output.cpp" line="9"/>
+        <location filename="../src/output.cpp" line="29"/>
         <source>Gamma correction</source>
         <translation>Gamma-Korrektur</translation>
     </message>
     <message>
-        <location filename="src/output.cpp" line="10"/>
+        <location filename="../src/output.cpp" line="30"/>
         <source>Brightness</source>
         <translation>Helligkeit</translation>
+    </message>
+    <message>
+        <location filename="../src/output.cpp" line="31"/>
+        <source>Enable/Disable</source>
+        <translation>Aktivieren/Deaktivieren</translation>
     </message>
 </context>
 </TS>
